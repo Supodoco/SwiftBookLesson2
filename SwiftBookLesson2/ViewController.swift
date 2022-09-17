f0785012080704ae8b030d54945fecdfdb7255ea
sss <kirillku4mar24@gmail.com>
2022-09-17 20:55:29 +0300

    Initial Commit

---
+++ SwiftBookLesson2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftBookLesson2
+//
+//  Created by Supodoco on 17.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
